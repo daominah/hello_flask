123bcff452c41373b574d0ba68a67ce65870c578
TungDT <tung.daothanhtung@gmail.com>
2019-11-06 21:00:49 +0700

    fix same name funcs

--- app.py
+++ app.py
@@ -4,21 +4,23 @@ app = Flask(__name__)
 
 
 @app.route('/hello')
-def hello1():
+def hello():
     return "hello"
 
 
 @app.route('/')
-def hello():
+def index():
     return render_template("index.html")
 
+
 @app.route('/haha')
-def hello():
+def haha():
     return "haha"
 
+
 @app.route('/hihi')
-def hello():
+def hihi():
     return render_template("hihi.html")
 
 
-app.run(port=5001, host="0.0.0.0")
+app.run(port=5002, host="0.0.0.0")
