9f03d4431545cd5a6e1669313329e5f81d756264
TungDT <tung.daothanhtung@gmail.com>
2019-11-06 18:03:55 +0700

    return html

--- app.py
+++ app.py
@@ -2,9 +2,45 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/hello')
+def hello1():
+    return "hello"
+
 @app.route('/')
 def hello():
-    return "Hello World! 1"
+    return """<html>
 
-if __name__ == '__main__':
-    app.run(port=5001)
+<head>
+    <title>
+        A Simple HTML Document
+    </title>
+</head>
+
+<body>
+    <div id="main">
+        The main div
+        <p>This is a very simple HTML document</p>
+        <p>It only has two paragraphs</p>
+        <a href="https://www.w3schools.com">This is a link</a>
+        <a href="https://www.google.com.vn#frag2" id="xxx2">Another link</a>
+        <a href="https://www.google.com.vn" class="c1">Link3</a>
+        <a href="https://www.google.com.vn#frag3" class="c2">Link4</a>
+        <img src="https://www.google.com.vn/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png">
+        <div id="dev_page_content_wrap" class=" ">
+            <h1>This is dev_page_content_wrap_h1</h1>
+            <a href="https://www.google.com.vn" class="c1">Link5</a>
+            <a href="https://www.google.com.vn" class="c2">Link6</a>
+            <div class="blog_wide_image">
+                <a href="/file/811140722/2/3cY8obIVMwk.102339/19827a3ef0493d9816" target="_blank">
+                    <img src="/file/811140722/2/3cY8obIVMwk.102339/19827a3ef0493d9816" title="Themes for Android" />
+                </a>
+            </div>
+        </div>
+    </div>
+    <p id="ahihi"> Not in main paragrap </p>
+	
+</body>
+
+</html>"""
+
+app.run(port=5001, host="0.0.0.0")
