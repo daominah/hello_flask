ea695f5b963bade909dd4be60553e72817656ee6
TungDT <tung.daothanhtung@gmail.com>
2019-11-06 21:07:29 +0700

    add image

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
@@ -21,6 +21,12 @@ def haha():
 @app.route('/hihi')
 def hihi():
     return render_template("hihi.html")
+
+
+@app.route('/i1')
+def handleI1():
+    path = "images/i1.jpeg"
+    return send_file(path, mimetype='image/jpeg')
 
 
 app.run(port=5002, host="0.0.0.0")
